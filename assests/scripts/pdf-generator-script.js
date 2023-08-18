5712bc567e9bb8b28e9f42d241addccb86a0ff3e
Rob S <me@rob-saunders.co.uk>
2023-08-18 23:45:42 +0100

    variables created and math functions added

--- assests/scripts/pdf-generator-script.js
+++ assests/scripts/pdf-generator-script.js
@@ -60,3 +60,77 @@ pdf
 }
 
 create()
+
+
+
+
+
+// PDF Invoice Generator
+
+/* PDF split into sections for easy variable creation (The below will be objects of data): 
+
+    1) Invoice credentials:
+        Date
+        Invoice #
+        Summary
+        Bill to Company
+        Bill to Company Email
+
+    2) Invoice Description (Multiple lines - Array of objects)
+        Description
+        Amount
+        Deposit 
+        Discount (can be used for discount)
+
+
+    3) Math
+        Subtotal
+        Total Due (Subtotal Less Deposit, Less Other)
+
+    4) Bank Details
+        Bank
+        Account Name
+        Sort Code
+        Account Number
+
+*/
+
+var credentials = [
+    {
+        date: "18/07/2023",
+        invoiceNo: "000121",
+        summary: "SEO Work August 2023",
+        billTo: "Meladore",
+        billEmail: "info@meladoredevelopments.co.uk"
+    }
+];
+
+const description = [
+    {
+        invoiceLines: [
+            {
+                description: "Lots and lots of work that is the best",
+                amount: 175
+            },
+            {
+                description: "Another load of stuff",
+                amount: 254
+            },
+        ],
+    },
+    {
+        deposit: 50,
+        discount: 10, 
+    },
+];
+
+var subTotal = addition(description[0].invoiceLines);
+var totalDue = subTotal - description[1].deposit - description[1].discount
+
+
+
+function addition (calc) {
+    let x = 0
+    calc.forEach((e) => x += e.amount)
+    return x;
+}
