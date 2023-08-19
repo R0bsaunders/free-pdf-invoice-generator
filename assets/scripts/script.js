6297fe005c2fade72d20a613a91ffd57fa7ae9a2
Rob S <me@rob-saunders.co.uk>
2023-08-19 12:43:20 +0100

    after much researched I have managed to encode the data sent to the URL in a way it can be decoded by the Node system

---
+++ assets/scripts/script.js
@@ -0,0 +1,85 @@
+const newRequest = [
+    {
+        date: "",
+        invoiceNo: 0,
+        summary: "",
+        billTo: "",
+        billEmail: "",
+        bankName: "",
+        accountName: "",
+        sortCode: "",
+        accountNumber: "",
+        deposit: 0,
+        discount: 0, 
+        subTotal: 0,
+        totalDue: 0,
+    },
+    {
+        invoiceLines: [
+            {
+                description: "",
+                amount: 0
+            },
+            {
+                description: "",
+                amount: 0
+            },
+        ],
+    },
+];
+
+// 3) Math
+
+var subTotal = addition(newRequest[1].invoiceLines);
+var totalDue = subTotal - newRequest[0].deposit - newRequest[0].discount;
+
+function addition (calc) {
+
+    let x = 0
+    calc.forEach((e) => x += e.amount)
+    return x;
+};
+
+
+
+
+
+const kopl = [
+    {
+        date: "05/89/65",
+        invoiceNo: 123456789,
+        summary: "BLABLABLABL",
+        billTo: "Me",
+        billEmail: "YoYo@ghj.com",
+        bankName: "TLloyds",
+        accountName: "Rob",
+        sortCode: "50-45-00",
+        accountNumber: "1234567",
+        deposit: 25,
+        discount: 25, 
+        subTotal: 1300,
+        totalDue: 1800,
+    },
+    {
+        invoiceLines: [
+            {
+                description: "Higgedlfy",
+                amount: 75
+            },
+            {
+                description: "Higgedlfy",
+                amount: 75
+            },
+            {
+                description: "Higgedlfy",
+                amount: 75
+            },
+            {
+                description: "Higgedlfy",
+                amount: 75
+            },
+        ],
+    },
+]
+
+let g = encodeURIComponent(JSON.stringify(kopl));
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-var pdf = require("pdf-creator-node");
-var fs = require("fs");
-var html = fs.readFileSync("pdf-template.html", "utf8");
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 // Server
@@ -10,9 +10,10 @@ var html = fs.readFileSync("pdf-template.html", "utf8");
 app.set("view engine", "ejs");
 
 app.get('/make-me-a-pdf/:data', (req, res) => {
-    pdfGenerator(req.params.data)
     console.log(req.params.data);
-    res.send("Done")
+    let details = JSON.parse(decodeURIComponent(req.params.data))
+    pdfGenerator(details)
+    res.send(details)
     
 })
 
@@ -20,109 +21,120 @@ app.listen(3000);
 
 
 function pdfGenerator (data) {
-
-var options = {
-    format: "A4",
-    orientation: "portrait",
-    border: "10mm",
-    header: {
-        height: "45mm",
-        contents: '<div style="text-align: center;">Author: Yo Momma</div>'
-    },
-    footer: {
-        height: "28mm",
-        contents: {
-            first: 'Cover page',
-            2: 'Second page', // Any page number is working. 1-based index
-            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-            last: 'Last Page'
+    var pdf = require("pdf-creator-node");
+    var fs = require("fs");
+    var html = fs.readFileSync("pdf-template.html", "utf8");
+    var options = {
+        format: "A4",
+        orientation: "portrait",
+        border: "10mm",
+        header: {
+            height: "45mm",
+            contents: '<div style="text-align: center;">Author: Yo Momma</div>'
+        },
+        footer: {
+            height: "28mm",
+            contents: {
+                first: 'Cover page',
+                2: 'Second page', // Any page number is working. 1-based index
+                default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+                last: 'Last Page'
+            }
         }
-    }
-};
-
-
-// 1) Invoice Credentials
-
-var credentials = {
-
-        date: "",
-        invoiceNo: data,
-        summary: "",
-        billTo: "",
-        billEmail: ""
-    }
-
-
-// 2) Invoice Description
-
-var description = [
-    {
-        invoiceLines: [
-            {
-                description: "",
-                amount: 0
-            },
-            {
-                description: "",
-                amount: 0
-            },
-        ],
-    },
-    {
-        deposit: 0,
-        discount: 0, 
-    },
-];
-
-// 3) Math
-
-var subTotal = addition(description[0].invoiceLines);
-var totalDue = subTotal - description[1].deposit - description[1].discount;
-
-function addition (calc) {
-
-    let x = 0
-    calc.forEach((e) => x += e.amount)
-    return x;
-};
-
-// 4) Bank Details
-
-var bankDetails = 
-    {
-        bankName: "",
-        accountName: "",
-        sortCode: "",
-        accountNumber: "",
     };
 
-var document = {
-    html: html,
-    data: {
-        credentials: credentials,
-        lines: description[0].invoiceLines,
-        description: description[1],
-        subTotal: subTotal,
-        total: totalDue,
-        bank: bankDetails
-    },
-    path: "invoice-"+credentials.invoiceNo+".pdf",
-    type: "",
-};
-// By default a file is created but you could switch between Buffer and Streams by using "buffer" or "stream" respectively.
 
-function create () {
+    // 1) Invoice Credentials
+    var credentials = {
 
-pdf
-  .create(document, options)
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+        date: data[0].date,
+        invoiceNo: data[0].invoiceNo,
+        summary: data[0].summary,
+        billTo: data[0].billTo,
+        billEmail: data[0].billEmail,
+    };
 
-};
-create()
+    // 2) Invoice Description
+    var description = [
+        
+            data[1].invoiceLines,
+        
+        {
+            deposit: data[0].deposit,
+            discount: data[0].discount, 
+        },
+    ];
 
-};
+
+
+    // 4) Bank Details
+
+    var bankDetails = 
+        {
+            bankName: data[0].bankName,
+            accountName: data[0].accountName,
+            sortCode: data[0].sortCode,
+            accountNumber: data[0].accountNumber,
+        };
+
+    var document = {
+        html: html,
+        data: {
+            credentials: credentials,
+            lines: description[0].invoiceLines,
+            description: description[1],
+            subTotal: data[0].subTotal,
+            total: data[0].totalDue,
+            bank: bankDetails
+        },
+        path: "invoice-"+credentials.invoiceNo+".pdf",
+        type: "",
+    };
+    // By default a file is created but you could switch between Buffer and Streams by using "buffer" or "stream" respectively.
+
+    function create () {
+
+    pdf
+    .create(document, options)
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+    };
+    create()
+
+    };
+
+
+    const newRequest = [
+        {
+            date: "",
+            invoiceNo: 0,
+            summary: "",
+            billTo: "",
+            billEmail: "",
+            bankName: "",
+            accountName: "",
+            sortCode: "",
+            accountNumber: "",
+            deposit: 0,
+            discount: 0, 
+            subTotal: 0,
+            totalDue: 0,
+        },
+        {
+            invoiceLines: [
+                {
+                    description: "",
+                    amount: 0
+                },
+                {
+                    description: "",
+                    amount: 0
+                },
+            ],
+        },
+];
