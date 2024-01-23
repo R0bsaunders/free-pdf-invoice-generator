2277d114e71ca8fb3e7c3c7c1629ce6dfd52053c
R0bsaunders <me@rob-saunders.co.uk>
2024-01-23 23:35:15 +0000

    added vercel json back and port number

--- main.js
+++ main.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
 
-console.log("Server is running at: http://127.0.0.1:3000");
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
