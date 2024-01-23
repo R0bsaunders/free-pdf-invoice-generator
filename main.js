7ae54a8d1402a3d4c27371108ab5ae376483081c
R0bsaunders <me@rob-saunders.co.uk>
2024-01-23 23:32:49 +0000

    Changed port again

--- main.js
+++ main.js
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(1);
+app.listen(3000);
 
-console.log("Server is running at: http://127.0.0.1:1");
+console.log("Server is running at: http://127.0.0.1:3000");
