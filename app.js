051d7e0c867f6ca0144335d186183daf52e5824c
R0bsaunders <me@rob-saunders.co.uk>
2024-01-31 22:28:08 +0000

    changed port to 3000

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const websitePort = process.env.PORT || 5500;
+const websitePort = process.env.PORT || 3000;
 const path = require('path')
 
 console.log("Hello");
